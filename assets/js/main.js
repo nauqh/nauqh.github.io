1c159a534cc3c39c24f32c8b5c407ff6be5be64e
Nauqh <hodominhquan.work@gmail.com>
2024-01-17 19:43:42 +0700

    edit content

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,39 +49,14 @@ function activeWork() {
 }
 linkWork.forEach((l) => l.addEventListener("click", activeWork));
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll("section[id]");
-
-function scrollActive() {
-	const scrollY = window.pageYOffset;
-
-	sections.forEach((current) => {
-		const sectionHeight = current.offsetHeight,
-			sectionTop = current.offsetTop - 58,
-			sectionId = current.getAttribute("id");
-
-		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-			document
-				.querySelector(".nav__menu a[href*=" + sectionId + "]")
-				.classList.add("active-link");
-		} else {
-			document
-				.querySelector(".nav__menu a[href*=" + sectionId + "]")
-				.classList.remove("active-link");
-		}
-	});
-}
-window.addEventListener("scroll", scrollActive);
-
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
 	origin: "top",
 	distance: "60px",
 	duration: 2500,
-	delay: 400,
 });
 
 sr.reveal(`.home__data`);
-sr.reveal(`.home__handle`, { delay: 700 });
-sr.reveal(`.home__social, .home__scroll`, { delay: 900, origin: "bottom" });
-sr.reveal(`.about`, { delay: 1200 });
+sr.reveal(`.home__handle`, { delay: 500 });
+sr.reveal(`.home__social, .home__scroll`, { delay: 700, origin: "bottom" });
+sr.reveal(`.about`, { delay: 1000 });
