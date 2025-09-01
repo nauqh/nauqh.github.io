8143538877565a9e06bb1e2c90dec4e4f44286e1
nauqh <hodominhquan.work@gmail.com>
2025-09-02 04:48:44 +1000

    update(rag): add new documents for vdb

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,23 +95,24 @@ modalClose.forEach((mc) => {
 });
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
-let mixerPortfolio = mixitup(".work__container", {
-	selectors: {
-		target: ".work__card",
-	},
-	animation: {
-		duration: 300,
-	},
-});
+// Remove or comment out the mixitup initialization
+// let mixerPortfolio = mixitup(".work__container", {
+//     selectors: {
+//         target: ".work__card",
+//     },
+//     animation: {
+//         duration: 300,
+//     },
+// });
 
-/* Link active work */
-const linkWork = document.querySelectorAll(".work__item");
+// Remove the filter click handlers
+// const linkWork = document.querySelectorAll(".work__item");
 
-function activeWork() {
-	linkWork.forEach((l) => l.classList.remove("active-work"));
-	this.classList.add("active-work");
-}
-linkWork.forEach((l) => l.addEventListener("click", activeWork));
+// function activeWork() {
+//     linkWork.forEach((l) => l.classList.remove("active-work"));
+//     this.classList.add("active-work");
+// }
+// linkWork.forEach((l) => l.addEventListener("click", activeWork));
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
